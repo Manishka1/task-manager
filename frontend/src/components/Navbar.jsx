922f906ef951214f54d9ecab49546b8654742977
Manishka1 <manishka110518@gmail.com>
2026-03-06 15:41:17 +0530

    fix navbar build error

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../features/auth/authSlice';
 import { useHistory } from 'react-router-dom';
-import { fetchTaskStats } from '../features/tasks/tasksSlice';
 
 export default function Navbar() {
+
   const dispatch = useDispatch();
   const history = useHistory();
-  const { user } = useSelector((s) => s.auth);
-  const { stats } = useSelector((s) => s.tasks);
 
-  useEffect(() => {
-  if (user) dispatch(fetchTaskStats());
-}, [dispatch, user]);
+  const { user } = useSelector((state) => state.auth);
 
   const handleLogout = () => {
     dispatch(logout());
@@ -23,6 +19,7 @@ export default function Navbar() {
   return (
     <AppBar position="static">
       <Toolbar>
+
         <Typography
           variant="h6"
           sx={{ flexGrow: 1, cursor: 'pointer' }}
@@ -32,34 +29,56 @@ export default function Navbar() {
         </Typography>
 
         {user && (
-          <Box sx={{ mr: 3 }}>
-            <Button color="inherit" onClick={() => history.push('/pie-chart')}>
-              View Pie Chart
-            </Button>
-          </Box>
+          <Button
+            color="inherit"
+            onClick={() => history.push('/pie-chart')}
+          >
+            Pie Chart
+          </Button>
         )}
 
         {user ? (
           <Box>
+
             {user.role === 'admin' && (
-              <Button color="inherit" onClick={() => history.push('/users')}>
+              <Button
+                color="inherit"
+                onClick={() => history.push('/users')}
+              >
                 Users
               </Button>
             )}
-            <Button color="inherit" onClick={handleLogout}>
+
+            <Button
+              color="inherit"
+              onClick={handleLogout}
+            >
               Logout
             </Button>
+
           </Box>
         ) : (
+
           <Box>
-            <Button color="inherit" onClick={() => history.push('/login')}>
+
+            <Button
+              color="inherit"
+              onClick={() => history.push('/login')}
+            >
               Login
             </Button>
-            <Button color="inherit" onClick={() => history.push('/register')}>
+
+            <Button
+              color="inherit"
+              onClick={() => history.push('/register')}
+            >
               Register
             </Button>
+
           </Box>
+
         )}
+
       </Toolbar>
     </AppBar>
   );
