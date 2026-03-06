95facfb0dd456987f987f2f75726c221dd00377e
Manishka1 <manishka110518@gmail.com>
2026-03-06 15:06:32 +0530

    fixed deatails

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Box, Typography, Button } from '@mui/material';
+import { useHistory } from 'react-router-dom';
 
 export default function ProtectedRoute({ component: Component, ...rest }) {
   const token = localStorage.getItem('token');
+  const history = useHistory();
+
   return (
     <Route
       {...rest}
-      render={props =>
-        token
-          ? <Component {...props}/>
-          : <Redirect to="/login"/>
+      render={(props) =>
+        token ? (
+          <Component {...props} />
+        ) : (
+          <Box textAlign="center" mt={10}>
+            <Typography variant="h5" gutterBottom>
+              Please login to view tasks
+            </Typography>
+            <Button
+              variant="contained"
+              onClick={() => history.push('/login')}
+            >
+              Go to Login
+            </Button>
+          </Box>
+        )
       }
     />
   );
